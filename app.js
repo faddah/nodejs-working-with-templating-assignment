8878dbd28be2abcd5c70e019c3d8bbe7d2827c68
Faddah Wolf <my_biz@me.com>
2023-07-11 10:24:21 -0700

    Put all routing in app.js.

--- app.js
+++ app.js
@@ -5,17 +5,41 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const users = [];
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const usersData = require('./routes/users');
-const coRoutes = require('./routes/company');
+// const usersData = require('./routes/users');
+// const coRoutes = require('./routes/company');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
   
-app.use(usersData.routes);
-app.use(coRoutes);
+// app.use(usersData.routes);
+// app.use(coRoutes);
+
+app.get('/', (req, res, next) => {
+	res.render('company', {
+		pageTitle: 'Our Company',
+		path: '/',
+	});
+});
+
+app.post('/add-user', (req, res, next) => {
+	users.push({ name: req.body.username });
+	res.redirect('/users');
+});
+
+app.get('/users', (req, res, next) => {
+	console.log(users);
+	res.render('users', {
+		pageTitle: 'Our Users',
+		path: '/users',
+		hasUsers: users.length > 0,
+		users: users,
+	});
+});
 
 app.use((req, res, next) => {
 	res.status(404).render('404', {pageTitle: '404 - Page Not Found!', path: '/'});
